11d659898a272ea41a02282eb0b64d5ec413ab18
Leon <1134379234@qq.com>
2024-03-13 16:43:48 +0800

    Update FakeProperty.kt
    set model to BLA-AL00

--- xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeProperty.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/FakeProperty.kt
@@ -12,6 +12,7 @@ enum class Property(val entry: Pair<String, String>) {
     EMUI_VERSION("ro.build.version.emui" to "EmotionUI_8.0.0"),
     BRAND("ro.product.brand" to "Huawei"),
     MANUFACTURER("ro.product.manufacturer" to "HUAWEI"),
+    MODEL("ro.product.model" to "BLA-AL00"),
     MIUI_VERSION("ro.miui.ui.version.name" to "");
 
     val key: String
